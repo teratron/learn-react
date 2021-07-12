27832bd624d926b694783537acdeb401c8f30055
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2021-07-12 17:35:05 +0300

    Cat

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react'
 
 //import Hook from './Ref'
-import Hook from './Custom'
+//import Hook from './Custom'
 //import Hook from './Effect'
 //import Hook from './State'
 //import Hook from './Memo'
 //import Hook from './Context'
 //import Hook from './Collback'
+import MouseTracker from './Cat'
+
 
 function App() {
     return (
-        <Hook />
+        //<Hook />
+        <MouseTracker />
     )
 }
 
